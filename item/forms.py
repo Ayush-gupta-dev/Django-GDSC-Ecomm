d1f32ae81dafc9acfbac51a452bdc5a1f8505824
ayush-gupta-dev <ayushguptadev1@gmail.com>
2023-10-30 14:28:31 +0530

    V1/Project-comleted delete edit option added

--- item/forms.py
+++ item/forms.py
@@ -28,3 +28,28 @@ class NewItemForm(forms.ModelForm):
             })
         }
         
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields=('category','priority_tag','name','description','price','image')
+        widgets={
+            'category': forms.Select(attrs={
+                'class':FORM_CLASS
+            }),
+            'priority_tag': forms.Select(attrs={
+                'class':FORM_CLASS
+            }),
+            'name': forms.TextInput(attrs={
+                'class':FORM_CLASS
+            }),
+            'description': forms.Textarea(attrs={
+                'class':FORM_CLASS,'rows':6,'cols':20,'style': 'resize: none;'
+            }),
+            'price': forms.TextInput(attrs={
+                'class':FORM_CLASS
+            }),
+            'image': forms.FileInput(attrs={
+                'class':'w-full m-2 px-4 py-2 rounded-xl border bg-slate-200 text-slate-700'
+            })
+        }
+    
--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render,redirect
 from .models import Item
 from django.contrib.auth.decorators import login_required
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
 
@@ -30,4 +30,23 @@ def new(request):
         'title': 'Add New Item'
     })
 
+@login_required
+def edit(request,pk):
+    item = get_object_or_404(Item,pk=pk,created_by=request.user)
+    if request.method == "POST":
+        form=EditItemForm(request.POST,request.FILES,instance=item)
+        if form.is_valid:
+            item.save()
+            return redirect('item:detail',pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request,'item/form.html',{
+        'form':form,
+        'title': 'Edit Item'
+    })
 
+@login_required
+def delete(request,pk):
+    item = get_object_or_404(Item,pk=pk,created_by=request.user)
+    item.delete()
+    return redirect('myitems:index')
